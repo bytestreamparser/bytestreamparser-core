ef50ec684cb84d921f552c6f0f08520216841eb9
Lin Yang <coding@lyang.dev>
2024-12-16 07:58:52 -0800

    Add GitHub CD release (#7)

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SOURCE="${SOURCE:-$(git rev-parse --abbrev-ref HEAD)}"
+TIMESTAMP="$(git log -1 --pretty=%cd --date=format:'%Y%m%d%H%M%S')"
+COMMIT="$(git rev-parse --short HEAD)"
+REVISION="${REVISION:-0.$SOURCE.$TIMESTAMP.$COMMIT}"
+
+mvn --define revision=$REVISION --batch-mode clean deploy 1>&2
+
+echo "REVISION=$REVISION"
