a0a6f7468a159cb7814fe7dd921c1de53316c643
Lin Yang <coding@lyang.dev>
2024-12-16 22:41:03 -0800

    Make major version environment variable

--- release.sh
+++ release.sh
@@ -3,7 +3,8 @@
 SOURCE="${SOURCE:-$(git rev-parse --abbrev-ref HEAD)}"
 TIMESTAMP="$(git log -1 --pretty=%cd --date=format:'%Y%m%d%H%M%S')"
 COMMIT="$(git rev-parse --short HEAD)"
-REVISION="${REVISION:-0.$SOURCE.$TIMESTAMP.$COMMIT}"
+MAJOR="${MAJOR:-0}"
+REVISION="${REVISION:-$MAJOR.$SOURCE.$TIMESTAMP.$COMMIT}"
 
 mvn --define revision="$REVISION" --batch-mode clean deploy 1>&2
 
