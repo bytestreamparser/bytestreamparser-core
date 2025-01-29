37516bffa93301ab419d78556ebf7a74766e338a
Lin Yang <coding@lyang.dev>
2025-01-29 10:13:04 -0800

    Make DataObject implementation more flexible

---
+++ composite-parser/src/test/java/org/bytestreamparser/composite/data/TestDataObject.java
@@ -0,0 +1,9 @@
+package org.bytestreamparser.composite.data;
+
+import java.util.HashMap;
+
+public class TestDataObject extends AbstractDataObject<TestDataObject> {
+  public TestDataObject() {
+    super(new HashMap<>());
+  }
+}
